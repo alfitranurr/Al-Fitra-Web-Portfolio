0918dd70c0817868fa5ef1e18498ba85d07afcc3
alfitranurr <alfitranurr@gmail.com>
2025-02-10 12:37:39 +0700

    . fix Police line white purple position

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -95,13 +95,13 @@ const Home: React.FC = () => {
       <div
         className="w-full h-8 bg-white mt-4 mb-[-35px] overflow-hidden flex items-center justify-center shadow-lg"
         style={{
-          transform: "rotate(-1deg) translateY(-10px) translateX(-2px)", // Rotate the box by 45 degrees
+          transform: "rotate(0deg) translateY(15px) translateX(-2px)", // Rotate the box by 45 degrees
           transformOrigin: "center", // Ensure the rotation is centered
         }}
       >
         <motion.div
           className="flex items-center justify-start space-x-50 text-xs sm:text-sm md:text-sm font-semibold text-gray-800 text-center whitespace-nowrap"
-          animate={isLoaded ? { x: ["58%", "-30%"] } : {}} // Trigger animation only after loading
+          animate={isLoaded ? { x: ["30%", "-30%"] } : {}} // Trigger animation only after loading
           transition={{
             repeat: Infinity, // Repeat the animation forever
             repeatType: "loop", // Loop the animation
@@ -126,13 +126,13 @@ const Home: React.FC = () => {
       <div
         className="w-full h-8 bg-[var(--warna2-color)] mt-0 mb-[-35px] overflow-hidden flex items-center justify-center px-0 shadow-lg"
         style={{
-          transform: "rotate(1deg) translateY(-20px) translateX(-2px)", // Adjust translateX to move the box further left
+          transform: "rotate(0deg) translateY(-20px) translateX(-2px)", // Adjust translateX to move the box further left
           transformOrigin: "center",
         }}
       >
         <motion.div
           className="flex items-center justify-start space-x-50 text-xs sm:text-sm md:text-sm font-semibold text-white text-center whitespace-nowrap"
-          animate={isLoaded ? { x: ["-58%", "30%"] } : {}} // Start from right and move left
+          animate={isLoaded ? { x: ["-30%", "30%"] } : {}} // Start from right and move left
           transition={{
             repeat: Infinity, // Repeat the animation forever
             repeatType: "loop", // Loop the animation
