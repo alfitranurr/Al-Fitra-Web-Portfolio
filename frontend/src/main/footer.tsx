bd7d326040ba4c7ea10281110af3fbbe3cef2f06
alfitranurr <alfitranurr@gmail.com>
2025-02-11 11:20:54 +0700

    .

--- frontend/src/main/footer.tsx
+++ frontend/src/main/footer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import * as React from "react";
 import { FaInstagram, FaWhatsapp, FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Footer: React.FC = () => {
--- frontend/src/main/navbar.tsx
+++ frontend/src/main/navbar.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import * as React from "react";
+import { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { animateScroll as scroll } from "react-scroll";
 
